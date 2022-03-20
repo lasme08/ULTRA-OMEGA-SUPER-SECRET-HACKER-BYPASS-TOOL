8a02c730feab3c6c2cffcc988e5cd4645d77fcaf
lasme08 <37305466+lasme08@users.noreply.github.com>
2022-03-20 23:23:19 +0200

    Add files via upload

---
+++ HACK_GOOGLE.bat
@@ -0,0 +1,502 @@
+@echo off
+VER
+title HACKING GOOGLE......
+color a
+echo PLEASE WAIT: HACKING GOOGLE
+timeout /t 2 /nobreak > nul
+echo RANSOM SENT: 1 DOLLAR (427146124736462367423 RUBLES)
+timeout /t 2 /nobreak > nul
+echo SERVER INFO COPIED: 103213123123 EXTABYTES
+timeout /t 2 /nobreak > nul
+echo BITCHES: ACQUIRED (3123)
+timeout /t 2 /nobreak > nul
+echo N-WORD PASS: ACQUIRED (2 YEARS)
+timeout /t 2 /nobreak > nul
+color c
+echo HACKING COMPLETE: GOOGLE IS YOURS LOLOLOLLOLOLOLLOLOLOL PRESS ANY KEY TO FUCK THEM UP!!!
+title GOOGLES FUCKED UP NOW!!!
+pause
+
+
+
+
+:start
+color 4
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+echo DESTROYING GOOGOL
+timeout /t 0 /nobreak > nul
+
+color c
+echo GOOGLE IS DEAD LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO PRESS ANY KEY TO REINSTALL GOOGLE, JUST TO DESTROY IT AGAIN XDDDDDDDDDDDDDDDDDDDD
+pause
+goto start
+
+
+
